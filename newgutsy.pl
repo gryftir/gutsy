a989a395fc63f3c9736a567829888aeb5f0bfed4
Lawrence Siebert <gryftir@gmail.com>
2013-07-02 23:41:55 -0700

    continuing work with treebuilder

--- newgutsy.pl
+++ newgutsy.pl
@@ -3,14 +3,14 @@ use strict;
 use HTML::TreeBuilder;
 
 my $localfile = "junehiring.html";
-
 open (my $filehandle, "<", $localfile) or die "$!\n";
 
 my $root = HTML::TreeBuilder->new_from_file($filehandle);
 
-my @tableelements = $root->look_down("_tag", "tr");
-foreach  my $table (@tableelements) {
-	if ($table->look_down("_tag", "img", "width", 0) ){
-		print $table->as_text(), "\n";
-		}
-	}
+#note to self, need to actually start with s.gif width=0
+my @users =  $root->look_down("_tag", "a", "href", qr/^user?/);
+shift @users; #get rid of who is hiring
+foreach my $user (@users) {
+$user->attr("href") =~ /^user\?id=(.*)/;
+		print $1, "\n", $user->depth(), "\n";
+}
