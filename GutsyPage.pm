d1520a04d9754b2832fa1151b60253d2426f9e57
Lawrence Siebert <gryftir@gmail.com>
2013-07-24 18:23:13 -0700

    can now pass a subroutine reference and use it to return an arbitrary selection of top level comments

--- GutsyPage.pm
+++ GutsyPage.pm
@@ -3,39 +3,58 @@ use strict;
 use warnings;
 use HTML::TreeBuilder;
 
+sub match_comments {
+    my $self     = shift;
+    my $function = shift;
+    my $arrayref = [];
+    foreach my $page ( @{ $self->{page} } ) {
+        my @temparray =
+          $page->look_down( "_tag", "img", "width", "0", "src", "s.gif" );
+        foreach my $post (@temparray) {
+            if ($post &&  $function->($post) ) {
+                push( @$arrayref, $post );
+            }
+        }
+    }
+    return $arrayref;
+}
+
 sub new_complete_url {
-	my $classname = shift;
-	my $url = shift;
-  my $self = GutsyPage->new_from_url($url);
-	for (my $index = 0; (my $next =  $self->has_more($index)) ; $index++) {
-		my $addurl = "https://news.ycombinator.com" . $next->attr("href");
-		$self->add_from_url($addurl);	
-	}
+    my $classname = shift;
+    my $url       = shift;
+    my $self      = GutsyPage->new_from_url($url);
+    for ( my $index = 0 ; ( my $next = $self->has_more($index) ) ; $index++ ) {
+        my $addurl = "https://news.ycombinator.com" . $next->attr("href");
+        $self->add_from_url($addurl);
+    }
+		return $self;
 }
 
 sub has_more {
-	my $self = shift;	
-	my $index = shift || "0";
-	return $self->{page}[$index]->look_down("_tag" => "a", "href" => qr/^\/x\?.*/); 
+    my $self = shift;
+    my $index = shift || "0";
+    return $self->{page}[$index]
+      ->look_down( "_tag" => "a", "href" => qr/^\/x\?.*/ );
 }
-
 
 sub download {
     my $classname = shift;
     my $url       = shift;
-    mkdir( ".files", "0755" ) unless -d ".files";
+    mkdir(".files") unless -d ".files";
     $url =~ /(\w+)$/;
-    system( "curl", "--silent", $url, "-o", ".files/$1.html" ) == 0
+    my $filesave = ".files/" . $1 . ".html";
+    system("curl --silent $url -o $filesave") == 0
       or die "$!\n curl failed\n";
-    return "$1.html";
+    return $1 . ".html";
 }
 
 sub add_from_url {
     my $self     = shift;
     my $url      = shift;
     my $filename = GutsyPage->download($url);
-    open( my $filehandle, "<", ".files/$filename" ) or die "$!\n";
-    shift @{ $self->{page} }, HTML::TreeBuilder->new_from_file($filehandle);
+    my $filesave = ".files/" . $filename;
+    open( my $filehandle, "<", $filesave ) or die "$!\n";
+    push @{ $self->{page} }, HTML::TreeBuilder->new_from_file($filehandle);
 
 }
 
@@ -52,7 +71,8 @@ sub new_from_filehandle {
 sub new_from_filename {
     my $classname = shift;
     my $filename  = shift;
-    open( my $filehandle, "<", ".files/$filename" ) or die "$!\n";
+    my $filesave  = ".files/" . $filename;
+    open( my $filehandle, "<", $filesave ) or die "$!\n";
     my $self = {};
     $self->{page} = [];
     $self->{page}[0] = HTML::TreeBuilder->new_from_file($filehandle);
@@ -67,9 +87,10 @@ sub new_from_url {
     my $nocurl    = shift;
     my $self      = {};
     if ( !$nocurl ) {    #if we didn't say don't use curl
-        if ( system("which curl 1> /dev/null") ) {
+        if ( !system("which curl 1> /dev/null") ) {
             my $filename = GutsyPage->download($url);
-            $self = GutsyPage->new_from_filename(".files/$filename");
+
+            $self = GutsyPage->new_from_filename($filename);
         }
         else { die "$!\n no curl on system\n"; }
     }
--- gutsy.pl
+++ gutsy.pl
@@ -2,25 +2,39 @@
 use strict;
 use HTML::TreeBuilder;
 use GutsyPage;
-my $localfile = "junehiring.html";
-open (my $filehandle, "<", $localfile) or die "$!\n";
-my @pages; #for the More Pages.  Note class="title" 2 on first page q, 1 on intermediate page, none on last page.
-my $gutsypage = GutsyPage->new_from_filehandle($filehandle);
+my $start_url = "https://news.ycombinator.com/item?id=5803764";
+my @pages
+  ; #for the More Pages.  Note class="title" 2 on first page q, 1 on intermediate page, none on last page.
+my $gutsypage = GutsyPage->new_complete_url($start_url);
 
-
-my @posts = $gutsypage->{page}[0]->look_down("_tag", "img", 
-	"width", "0",
-	"src", "s.gif");
-foreach my $post (@posts) {
-	my @line = $post->lineage();
-	my $user = $line[0]->right();
-	if ($user) {
-		$user = $user->right()->look_down("_tag", "a", "href", qr/^user?/);
-		my $font = $user->look_up("_tag", "td")->look_down("_tag", "font");
-		if ($font->as_text()	=~ /[[:^alpha:]](intern|internship)[sS]?[[:^alpha:]]/i) {
-			$user->attr("href") =~ /^user\?id=(.*)/;
-			print"\nuser ", $1, "\n\n";
-			print $font->as_text(), "\n";
+my $posts = $gutsypage->match_comments(sub  {
+        my $post = shift;
+        my @line = $post->lineage();
+        my $user = $line[0]->right();
+        if ($user) {
+            $user =
+              $user->right()->look_down( "_tag", "a", "href", qr/^user?/ );
+            my $font =
+              $user->look_up( "_tag", "td" )->look_down( "_tag", "font" );
+            if (   $font
+                && $font->as_text() =~
+                /[[:^alpha:]](intern|internship)[sS]?[[:^alpha:]]/i )
+            {
+                return 1;
+            }
+        }
+        return 0;
+			});
+foreach my $post (@$posts) {
+    my @line = $post->lineage();
+    my $user = $line[0]->right();
+    if ($user) {
+        $user = $user->right()->look_down( "_tag", "a", "href", qr/^user?/ );
+        my $font = $user->look_up( "_tag", "td" )->look_down( "_tag", "font" );
+        $user->attr("href") =~ /^user\?id=(.*)/;
+        print "\nuser ", $1, "\n\n";
+        print $font->as_text(), "\n";
+			}
 		}
-	}
-}
+	
+
